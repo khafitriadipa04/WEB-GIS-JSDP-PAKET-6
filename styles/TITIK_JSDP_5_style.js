87c4ab0ee4ea6cfc145544a7a828d67fc01470d1
JSDP ZONA 1 <khafi.triadipa@gmail.com>
2025-11-13 15:56:59 +0700

    Add files via upload

--- styles/TITIK_JSDP_5_style.js
+++ styles/TITIK_JSDP_5_style.js
@@ -1,5 +1,36 @@
 var size = 0;
 var placement = 'point';
+function categories_TITIK_JSDP_5(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement) {
+                var valueStr = (value !== null && value !== undefined) ? value.toString() : 'default';
+                switch(valueStr) {case 'BELUM ADA PEKERJAAN':
+                    return [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 6.0 + size,
+            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(61,128,53,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(0,0,0,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'DALAM PROSES':
+                    return [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 6.0 + size,
+            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(61,128,53,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(227,26,28,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'SELESAI - CATATAN (COVER MANHOLE)':
+                    return [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 6.0 + size,
+            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(61,128,53,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(254,255,0,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;}};
 
 var style_TITIK_JSDP_5 = function(feature, resolution){
     var context = {
@@ -8,7 +39,7 @@ var style_TITIK_JSDP_5 = function(feature, resolution){
     };
     
     var labelText = ""; 
-    var value = feature.get("");
+    var value = feature.get("SURVEY JSDP PAKET 6  PROGRES PEKERJAAN");
     var labelFont = "9.1px \'MS Shell Dlg 2\', sans-serif";
     var labelFill = "#000000";
     var bufferColor = "#ffffff";
@@ -20,13 +51,10 @@ var style_TITIK_JSDP_5 = function(feature, resolution){
     if (feature.get("Name") !== null) {
         labelText = String(feature.get("Name"));
     }
-    var style = [ new ol.style.Style({
-        image: new ol.style.Circle({radius: 6.0 + size,
-            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(61,128,53,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(227,26,28,1.0)'})}),
-        text: createTextStyle(feature, resolution, labelText, labelFont,
-                              labelFill, placement, bufferColor,
-                              bufferWidth)
-    })];
+    
+    var style = categories_TITIK_JSDP_5(feature, value, size, resolution, labelText,
+                            labelFont, labelFill, bufferColor,
+                            bufferWidth, placement);
 
     return style;
 };
